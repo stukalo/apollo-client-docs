1544f7336560f6d2e8525979ab588f05878e73dd
StukaloAV <alexandrst@wizardsdev.com>
2019-04-08 12:41:46 +0300

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'apollo-client',
+            template: './src/index.html'
+        }),
+        new FaviconsWebpackPlugin('./assets/icons/favicon.png')
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: ['.js', '.jsx'],
+                },
+                use: [{
+                    loader: "babel-loader"
+                }]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ],
+            },
+        ]
+    }
+};
